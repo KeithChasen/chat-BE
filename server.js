05d10b283a45fbcaa22c335d1e799d9561a2c14e
keithchasen <keithchasen89@gmail.com>
2021-06-08 19:59:30 +0300

    added mongo

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const fs = require('fs');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
+
+const config = fs.existsSync('./config.js') ? require('./config.js') : null;
+
+const MONGO = process.env.MONGO || config.MONGO;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers
 });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+mongoose
+  .connect(MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Mongo is connected');
+    return server.listen().then(({ url }) => {
+      console.log(`Server ready at ${url}`);
+    })
+  })
+  .catch(err => console.error(err));
