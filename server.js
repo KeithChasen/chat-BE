6e6b7496925ee7369d19702157355b5218d3dca9
keithchasen <keithchasen89@gmail.com>
2021-06-08 18:53:13 +0300

    get users query

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`  
+  type User {
+      username: String!
+      email: String!
+  }
+  
   type Query {
-    hello: String
+    getUsers: [User]!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'hello',
+    getUsers: () => {
+      return [{ id: 1, username: 'john', email: 'john@mail.com'}, { id: 2, username: 'jack', email: 'jack@mail.com'}];
+    },
   },
 };
 
