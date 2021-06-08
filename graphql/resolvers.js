06a5105631bf6bcc01cb6e8bcdda8b3d88a7f903
keithchasen <keithchasen89@gmail.com>
2021-06-08 20:08:57 +0300

    user model

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,13 @@
+const User = require('../mongo/User');
+
 module.exports = {
   Query: {
-    getUsers: () => {
-      return [{ id: 1, username: 'john', email: 'john@mail.com'}, { id: 2, username: 'jack', email: 'jack@mail.com'}];
+    getUsers: async () => {
+      try {
+        return await User.find();
+      } catch (err) {
+        console.log(err)
+      }
     },
   },
 };
